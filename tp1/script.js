70e7f1f720f58fe686c1bd5f445570fabc67d23c
nikoloz941 <nferoiani@gmail.com>
2024-09-30 23:25:53 +0200

    Delete tp1/script.js

--- tp1/script.js
+++
@@ -1,5 +0,0 @@
-function chooseCharacter(character) {
-    alert("Vous avez choisi : " + character);
-    // Transitionner vers le jeu avec le personnage sélectionné
-    window.location.href = "game.html?character=" + character;
-}
